727e1415b3ac17080760376b548b2be5f8836ec2
Manish Lakhara <manishlakhara60402001@gmail.com>
2023-05-31 15:24:26 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+  jQuery('.menu-toggle').click(function(){
+    jQuery('html,body').toggleClass('open-menu')
+    jQuery('.nav-wrapper').toggleClass('active')
+    jQuery('.menu-toggle').toggleClass('active')
+  });
+  jQuery('.nav-link').click(function(){
+    jQuery('.menu-toggle').removeClass('active')
+    jQuery('.nav-wrapper').removeClass('active')
+  });
+
+  $('.main-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll:2,
+    prevArrow:'<button class="slide-arrow prev-arrow"></button>',
+    nextArrow:'<button class="slide-arrow next-arrow"></button>',
+    variableWidth:true,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow:1,
+          slidesToScroll:2,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow:1,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+        
+  });
+})
+
+// const menuToggle = document.querySelector(".menu-toggle");
+// const navWrapper = document.querySelector(".nav-wrapper");
+
+// menuToggle.addEventListener("click",() => {
+//   menuToggle.classList.toggle("active");
+//   navWrapper.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".nav-link").forEach(n => n.
+//   addEventListener("click", () => {
+//     menuToggle.classList.remove("active");
+//     navWrapper.classList.remove("active");
+// }))
+
+  
